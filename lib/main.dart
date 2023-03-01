43bfacfa671c50dd6d7f7a48328edc79ff34e3e8
Artem GartService <art.garanskiy@gmail.com>
2023-03-02 07:30:09 +0800

    .env package was added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
+import 'package:dotenv/dotenv.dart';
 
+void main() {
+  var env=DotEnv(includePlatformEnvironment: true)..load();
+  env.isEveryDefined(['FOO']);
+  runApp(MyApp());
+  print('HELLO ${env['FOO']}');
 
-void main() => runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
